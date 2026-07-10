fd0a1cecf9ee94d405a0e23cb6265f297c58eb1b
hilmizul <netspytux@gmail.com>
2026-07-10 11:29:08 +0700

    add button portal Guru and Siswa

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,19 +1,19 @@
 <template>
-  <nav class="navbar fixed-top bg-white border-bottom border-2 border-dark">
-    <div class="container justify-content-center">
-      <NuxtLink class="navbar-brand lexend-mega mx-auto text-center">
-        <span class="py-1 px-2 text-bg-dark fs-4">e-PKL</span>
-        <span class="mt-1 quicksand smaller text-muted ms-2">SMKN 4 Tasikmalaya.</span>
+  <nav class="navbar navbar-expand fixed-top bg-white border-bottom border-2 border-dark">
+    <div class="container">
+      <NuxtLink class="navbar-brand lexend-mega mx-auto">
+        <span class="py-1 px-2 text-bg-dark fs-4">e-PKL</span> <br>
+        <span class="mt-1 quicksand smaller text-muted">SMKN 4 Tasikmalaya.</span>
       </NuxtLink>
-      <!-- <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation"> -->
-      <!--   <i class="bi bi-list-nested"></i> -->
-      <!-- </button> -->
-      <!-- <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup"> -->
-      <!--   <div class="navbar-nav"> -->
-      <!--     <NuxtLink to="/" class="nav-link text-dark fw-bold" aria-current="page">HOME</NuxtLink> -->
-      <!--     <NuxtLink to="/iduka" class="nav-link text-dark fw-bold" aria-current="page">IDUKA</NuxtLink> -->
-      <!--   </div> -->
-      <!-- </div> -->
+      <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="bi bi-list-nested"></i>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <NuxtLink to="https://guru.epkl.web.id" target="_blank" class="btn btn-success btn-sm border border-2 border-dark nav-link fw-bold" aria-current="page">Guru <i class="bi bi-box-arrow-up-right"></i></NuxtLink>
+          <NuxtLink to="https://siswa.epkl.web.id" target="_blank" class="btn btn-warning btn-sm border border-2 border-dark nav-link fw-bold" aria-current="page">Siswa <i class="bi bi-box-arrow-up-right"></i></NuxtLink>
+        </div>
+      </div>
     </div>
   </nav>
 </template>
