200dafb7f54f68c13774c71b6fd401b1212b3074
hilmizul <netspytux@gmail.com>
2026-07-10 16:25:44 +0700

    center of col

--- components/Permendikbud.vue
+++ components/Permendikbud.vue
@@ -1,28 +1,26 @@
 <template>
-  <div class="row pt-5">
-    <div class="col-md-12">
-      <figure class="text-center bg-white py-2">
-        <div class="quote">❝</div>
-        <blockquote class="blockquote fs-3">
-          <span class="fw-bold">Praktik Kerja Lapangan</span> (PKL) adalah
-          pembelajaran bagi Peserta Didik pada SMK/MAK, SMALB, dan LKP yang
-          dilaksanakan melalui praktik kerja di dunia kerja dalam jangka waktu tertentu sesuai
-          dengan kurikulum dan kebutuhan dunia kerja.
-        </blockquote>
-        <figcaption class="blockquote-footer">
-          Permendikbud Nomor 50 Tahun 2020 <cite title="Source Title">
-            <NuxtLink to="https://guru.kemendikdasmen.go.id/dokumen/WZ20mN6AdE?parentCategory=Implementasi%20Kurikulum%20Nasional" target="_blank" class="link-external text-muted">Panduan PKL</NuxtLink></cite>
-        </figcaption>
-      </figure>
+  <figure class="text-center bg-white py-2">
+    <div class="quote-container">
+      <div class="quote">❝</div>
     </div>
-  </div>
+    <blockquote class="blockquote fs-3">
+      <span class="fw-bold">Praktik Kerja Lapangan</span> (PKL) adalah
+      pembelajaran bagi Peserta Didik pada SMK/MAK, SMALB, dan LKP yang
+      dilaksanakan melalui praktik kerja di dunia kerja dalam jangka waktu tertentu sesuai
+      dengan kurikulum dan kebutuhan dunia kerja.
+    </blockquote>
+    <figcaption class="blockquote-footer">
+      Permendikbud Nomor 50 Tahun 2020 <cite title="Source Title">
+        <NuxtLink to="https://guru.kemendikdasmen.go.id/dokumen/WZ20mN6AdE?parentCategory=Implementasi%20Kurikulum%20Nasional" target="_blank" class="link-external text-muted">Panduan PKL</NuxtLink></cite>
+    </figcaption>
+  </figure>
 </template>
 
 <style scoped>
-figure {
+.quote-container {
   position: relative;
 }
-figure .quote {
+.quote-container .quote {
   position: absolute;
   top: -60px;
   left: -10px;
