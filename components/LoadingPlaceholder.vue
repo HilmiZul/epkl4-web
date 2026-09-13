1d6941b9fd5c30c722d6439af4ffdc4fac5cf623
hilmizul <netspytux@gmail.com>
2026-09-13 11:19:36 +0700

    move to app/ directory

--- components/LoadingPlaceholder.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <p v-for="(r,i) in props.row" :key="i" class="placeholder-glow mb-2">
-    <span :class="`placeholder col-${props.col} py-3`"></span>
-  </p>
-</template>
-
-<script setup>
-let props = defineProps(['col', 'row'])
-</script>
