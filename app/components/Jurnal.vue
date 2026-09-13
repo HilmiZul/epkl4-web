42c8bf33e8be22b00b7dc78ba10bd437277ec7e1
hilmizul <netspytux@gmail.com>
2026-09-13 11:17:48 +0700

    update structure directory based-on Nuxt 4.x

---
+++ app/components/Jurnal.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="col-md-4 smallest">
+    <div class="card mb-5">
+      <div class="card-body">
+        <div class="fs-3 fw-bold">Jurnal</div>
+        <div class="text-muted mb-3">Catatan aktivitas harian</div>
+
+        <div v-if="isLoading">
+          <LoadingPlaceholder :row="1" :col="4" />
+          <LoadingPlaceholder :row="3" :col="12" />
+          <LoadingPlaceholder :row="1" :col="4" />
+          <LoadingPlaceholder :row="3" :col="12" />
+        </div>
+
+        <div v-else-if="!isLoading && journals.length <= 0" class="text-center text-muted py-5">
+          <i class="bi bi-database fs-2"></i>
+          <div class="fs-5">Belum ada jurnal</div>
+        </div>
+
+        <ul v-else v-for="(item, i) in journals" :key="i" class="list-group">
+          <li class="list-group-item border-bottom border-bottom-1 border-bottom-grey rounded-0">
+            <div class="text-muted mb-2">{{ item.created }}</div>
+
+            <main class="pre-text">
+              {{ item.deskripsi }}
+            </main>
+          </li>
+        </ul>
+      </div>
+    </div> <!-- ./card -->
+  </div>
+</template>
+
+
+<script setup>
+const client = usePocketBaseClient()
+const isLoading = ref(false)
+const journals = ref([])
+
+onMounted(() => {
+  getJournals()
+})
+
+
+async function getJournals() {
+  isLoading.value = true
+
+  let res = await client.collection('jurnal_publik').getFullList({})
+
+  if(res) {
+    journals.value = res
+
+    // konversi waktu UTC dari server ke full date lokal indo
+    for(let i=0; i<journals.value.length; i++) {
+      const date = new Date(journals.value[i].created);
+      const options = {
+        dateStyle: "full",
+        timeStyle: "short"
+      };
+      journals.value[i].created = new Intl.DateTimeFormat('id-ID', options).format(date);
+    }
+
+    isLoading.value = false
+  }
+}
+</script>
---
+++ app/components/StatisticIduka.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="row">
+    <div class="col-md-4">
+      <NuxtLink to="/iduka" class="link">
+        <div class="alert">
+          <div class="row">
+            <!-- <div class="col-md-3"> -->
+            <!--   <div class="fs-2"><i class="bi bi-buildings-fill"></i></div> -->
+            <!-- </div> -->
+            <div class="col-md-9">
+              <LoadingPlaceholder v-if="isLoading" :row="1" :col="2" />
+              <div v-else class="fs-2 fw-bold">{{ iduka }}</div>
+              <div class="text-muted fw-bold">Total IDUKA <i class="bi bi-arrow-up-right-square"></i></div>
+            </div>
+          </div>
+        </div>
+      </NuxtLink>
+    </div>
+
+    <div class="col-md-4">
+      <div class="alert">
+        <div class="row">
+          <!-- <div class="col-md-3"> -->
+          <!--   <div class="fs-2"><i class="bi bi-globe"></i></div> -->
+          <!-- </div> -->
+
+          <div class="col-md-9">
+            <LoadingPlaceholder v-if="isLoadingLuarKota" :row="1" :col="2" />
+            <div v-else class="fs-2 fw-bold">{{ iduka_luar }}</div>
+            <div class="text-muted fw-bold">IDUKA Luar Kota</div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-4">
+      <div class="alert">
+        <div class="row">
+          <!-- <div class="col-md-3"> -->
+          <!--   <div class="fs-2"><i class="bi bi-geo-fill"></i></div> -->
+          <!-- </div> -->
+
+          <div class="col-md-9">
+            <LoadingPlaceholder v-if="isLoadingDalamKota" :row="1" :col="2" />
+            <div v-else class="fs-2 fw-bold">{{ iduka_dalam }}</div>
+            <div class="text-muted fw-bold">IDUKA Dalam Kota</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script setup vapor>
+const client = usePocketBaseClient()
+const isLoading = ref(false)
+const isLoadingLuarKota = ref(false)
+const isLoadingDalamKota = ref(false)
+
+const iduka = ref(0)
+const iduka_dalam = ref(0)
+const iduka_luar = ref(0)
+
+
+async function getData(collectionName) {
+  isLoading.value = true
+  isLoadingDalamKota.value = true
+  isLoadingLuarKota.value = true
+
+  try {
+    let res = await client.collection(collectionName).getList(1,1)
+
+    if(collectionName == 'count_iduka') {
+      iduka.value = res.items[0].jumlah_iduka
+      isLoading.value = false
+    } else if(collectionName == 'count_iduka_dalam') {
+      iduka_dalam.value = res.items[0].jumlah_iduka
+      isLoadingDalamKota.value = false
+    } else if(collectionName == 'count_iduka_luar') {
+      iduka_luar.value = res.items[0].jumlah_iduka
+      isLoadingLuarKota.value = false
+    }
+
+  } catch(error) {
+    console.error(error)
+    isLoading.value = false
+    isLoadingDalamKota.value = false
+    isLoadingLuarKota.value = false
+  }
+}
+
+
+onMounted(() => {
+  getData('count_iduka')
+  getData('count_iduka_dalam')
+  getData('count_iduka_luar')
+})
+</script>
